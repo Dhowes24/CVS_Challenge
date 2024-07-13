a61d26a496f3eab855c384af913d48e3f5bc48f1
Dhowes24 <d.howes24@>
2024-07-13 14:14:33 -0400

    Initial Commit

---
+++ CVS_Challenge/CVS_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVS_ChallengeApp.swift
+//  CVS_Challenge
+//
+//  Created by Derek Howes on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CVS_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVS_Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CVS_Challenge
+//
+//  Created by Derek Howes on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
